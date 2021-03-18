b6b1dcabcc922f2e42edf3a838752e4296410d63
NurAliia <battalova.nur@gmail.com>
2021-03-18 21:57:01 +0300

    Finish second test

--- Framework/src/test/java/ExampleTest.java
+++ Framework/src/test/java/ExampleTest.java
@@ -2,20 +2,20 @@ import org.testng.annotations.Test;
 
 public class ExampleTest extends A_BaseTest
 {
-//    @Test
-//    public void loginViaEmail() {
-//        app.loginPage.open();
-//        app.loginPage.login("abc", "abc@mail.ru", "123");
-//        app.loginPage.logout();
-//
-//        logger.info("Sample info message");
-//        logger.warn("Sample warn message");
-//        logger.error("Sample error message");
-//        logger.fatal("Sample fatal message");
-//
-//        softAssert.assertEquals(2,2);
-//        softAssert.assertAll();
-//    }
+    @Test
+    public void loginViaEmail() {
+        app.loginPage.open();
+        app.loginPage.login("abc", "abc@mail.ru", "123");
+        app.loginPage.logout();
+
+        logger.info("Sample info message");
+        logger.warn("Sample warn message");
+        logger.error("Sample error message");
+        logger.fatal("Sample fatal message");
+
+        softAssert.assertEquals(2,2);
+        softAssert.assertAll();
+    }
 
     @Test
     public void gadgets() {
