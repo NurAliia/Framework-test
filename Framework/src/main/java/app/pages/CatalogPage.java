29622ab49317ad452703617b78e35bcf0437ab69
NurAliia <battalova.nur@gmail.com>
2021-03-18 21:57:01 +0300

    Finish second test

--- Framework/src/main/java/app/pages/CatalogPage.java
+++ Framework/src/main/java/app/pages/CatalogPage.java
@@ -3,6 +3,7 @@ package app.pages;
 import com.codeborne.selenide.SelenideElement;
 import helpers.Driver;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.visible;
 
 public class CatalogPage extends BasePage {
 
@@ -11,6 +12,12 @@ public class CatalogPage extends BasePage {
     public SelenideElement sony = $("#li_br156");
     public SelenideElement description = $("#preset_t_18920");
     public SelenideElement nfc = $("#c17983~label");
+    public SelenideElement submit = $("#match_submit");
+    public SelenideElement camera = $("#product_918494");
+    public SelenideElement favorite = $("span.heart-empty");
+    public SelenideElement portfolio = $("#mui_user_login_row");
+    public SelenideElement favoriteList = $("div.touchcarousel-item a[href=\"/SONY-HDR-AS300.htm\"]");
+    public SelenideElement removeFavorite = $("div.touchcarousel-item div.whishlist-action--remove");
 
     public CatalogPage(String pageUrl) {
         super(pageUrl);
@@ -23,6 +30,14 @@ public class CatalogPage extends BasePage {
         sony.click();
         description.scrollTo();
         nfc.click();
-//        Driver.waitForUrlContains("idwu_=");
+        submit.scrollTo();
+        submit.click();
+        camera.click();
+        favorite.click();
+        portfolio.click();
+        favoriteList.shouldBe(visible);
+
+        $("div.touchcarousel-item").hover();
+        removeFavorite.click();
     }
 }
