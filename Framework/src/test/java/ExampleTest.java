692225c164484728d2cda2987d2fdffd9518ef5e
NurAliia <battalova.nur@gmail.com>
2021-03-23 19:07:57 +0300

    Add before and after tests

--- Framework/src/test/java/ExampleTest.java
+++ Framework/src/test/java/ExampleTest.java
@@ -1,18 +1,22 @@
-import org.testng.annotations.Test;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
-public class ExampleTest extends A_BaseTest
-{
+public class ExampleTest extends A_BaseTest {
+
+    @BeforeClass
+    public void beforeSuite()
+    {
+        app.loginPage.open();
+    }
+    @AfterClass
+    public void afterSuite()
+    {
+        app.loginPage.logout();
+    }
     @Test
     public void loginViaEmail() {
-        app.loginPage.open();
         app.loginPage.login("abc", "abc@mail.ru", "123");
-        app.loginPage.logout();
 
         logger.info("Sample info message");
-        logger.warn("Sample warn message");
-        logger.error("Sample error message");
-        logger.fatal("Sample fatal message");
 
         softAssert.assertEquals(2,2);
         softAssert.assertAll();
@@ -20,15 +24,10 @@ public class ExampleTest extends A_BaseTest
 
     @Test
     public void gadgets() {
-        app.loginPage.open();
         app.loginPage.login("abc", "abc@mail.ru", "123");
         app.catalogPage.gadgets();
-        app.loginPage.logout();
 
         logger.info("Sample info message");
-        logger.warn("Sample warn message");
-        logger.error("Sample error message");
-        logger.fatal("Sample fatal message");
 
         softAssert.assertEquals(2,2);
         softAssert.assertAll();
@@ -37,15 +36,10 @@ public class ExampleTest extends A_BaseTest
     @Test
     @Parameters({"maxPrice"})
     public void searchByPrice(String maxPrice) {
-        app.loginPage.open();
         app.loginPage.login("abc", "abc@mail.ru", "123");
         app.catalogPage.searchByPrice(maxPrice);
-        app.loginPage.logout();
 
         logger.info("Sample info message");
-        logger.warn("Sample warn message");
-        logger.error("Sample error message");
-        logger.fatal("Sample fatal message");
 
         softAssert.assertEquals(2,2);
         softAssert.assertAll();
